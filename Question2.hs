bf3c90bfdcbdbd8c4679e9c7c0e388ebe730d6b9
LenThava <dehlen@protonmail.ch>
2026-05-09 18:03:50 +0200

    Update Question2.hs
    worked over comments

--- Question2.hs
+++ Question2.hs
@@ -1,25 +1,25 @@
 
--- Question 2: Simple Infinite Lists
--- These lists are infinite. Because Haskell is lazy, only the parts requested
--- with take are actually evaluated. This is also explained in the PDF solution.
+-- Q2
+-- infinite lists: Haskell nature allows us to only evaluate "requested" parts
+-- noted in the pdf.
 
--- 2a)
--- Infinite list of positive multiples of 3.
+-- 2a
+-- infinite list for the positive mulitplier of 3
 multiplesOfThree :: [Integer]
 multiplesOfThree = [3, 6 ..]
 
--- 2b)
--- Infinite list of pairs containing a number and its square.
+-- 2b
+-- infinite list of pairs which have their number and its corresponding square
 numberSquarePairs :: [(Integer, Integer)]
 numberSquarePairs = [(n, n * n) | n <- [1 ..]]
 
--- 2c)
--- Numbers divisible by both 3 and 5 are exactly the multiples of 15.
+-- 2c
+-- those are multiples of 15 becausethey are divisible by 5 and 3
 divisibleByThreeAndFive :: [Integer]
 divisibleByThreeAndFive = [15, 30 ..]
 
 
--- Print a simple section heading for the test output.
+-- header for tests
 printSection :: String -> IO ()
 printSection title = do
   putStrLn ""
@@ -27,16 +27,16 @@ printSection title = do
   putStrLn (replicate (length title) '-')
 
 
--- Show finite prefixes of the infinite lists.
+-- we have the infinite lists here and the finite prefixes are shown
 main :: IO ()
 main = do
-  printSection "Question 2"
+  printSection "Q2"
 
-  putStrLn "2a) first 15 multiples of 3"
+  putStrLn "2a first 15 mult. of 3"
   print (take 15 multiplesOfThree)
 
-  putStrLn "2b) first 10 natural-number/square pairs"
+  putStrLn "2b first 10 int and square pairs"
   print (take 10 numberSquarePairs)
 
-  putStrLn "2c) first 15 numbers divisible by both 3 and 5"
+  putStrLn "2c first 15 numbers div. by 3 and 5"
   print (take 15 divisibleByThreeAndFive)
